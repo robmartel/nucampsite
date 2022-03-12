18e32f133fcd6159d926b1c3f823697970d0e44c
robmartel <99683835+robmartel@users.noreply.github.com>
2022-03-12 10:33:31 -0500

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,14 @@ $(function() {
         }
     });
 });
+
+$(function() {
+    $("#reserveButton").click(function(){
+        $('#reserveModal').modal('show')
+    });
+});
+$(function() {
+    $("#loginButton").click(function(){
+        $('#loginModal').modal('show')
+    });
+});
